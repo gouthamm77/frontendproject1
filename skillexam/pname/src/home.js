2bcea24bf84a2a1a13fe540afddb67ad8b7246f2
gouthamm77 <2400031906@kluniversity.in>
2025-10-16 11:13:52 +0530

    Update project files

---
+++ skillexam/pname/src/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page">
+      <h1>Welcome to Our Website</h1>
+      <p>This is the home page. We provide excellent courses and services.</p>
+      <div className="content">
+        <h2>About Us</h2>
+        <p>We are committed to providing quality education and training to our students.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
